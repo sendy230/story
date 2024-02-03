4ce691f9897f0fd387ba4c06270bacc5aaaaf4f5
Lullaby330 <69coder@gmail.com>
2024-02-03 13:17:50 +0700

    Update link_generator.py

--- plugins/link_generator.py
+++ plugins/link_generator.py
@@ -1,6 +1,4 @@
-# (©)Codexbotz
-# Recode by @mrismanaziz
-# t.me/SharingUserbot & t.me/Lunatic0de
+
 
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
