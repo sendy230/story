04b4c3881ba4d948c22e6eb17260a2ed2e639e0b
sendy230 <84791701+sendy230@users.noreply.github.com>
2024-03-26 11:56:01 +0700

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ from logging.handlers import RotatingFileHandler
 load_dotenv("config.env")
 
 # Bot token dari @Botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6350463915:AAGU3v0j_KUML6XzmyWCwH_nZtMJE2TbCYs")
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "5984069414:AAG4TZbO-VMw-7YJ2hgtrdbMKt6vkHD7MXw")
 
 # API ID Anda dari my.telegram.org
 APP_ID = int(os.environ.get("APP_ID", "6216349"))
@@ -17,7 +17,7 @@ APP_ID = int(os.environ.get("APP_ID", "6216349"))
 API_HASH = os.environ.get("API_HASH", "5c7418e9f3df6db931caa7354521c55f")
 
 # ID Channel Database
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002028434926"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001703681718"))
 
 # NAMA OWNER
 OWNER = os.environ.get("OWNER", "1823080600")
@@ -36,7 +36,7 @@ UPSTREAM_BRANCH = os.environ.get("UPSTREAM_BRANCH", "master")
 DB_URI = os.environ.get("DATABASE_URL", "postgres://zlwrrqqh:3oTF60FLAqCyis0auMcnZW2RSJfMq2qf@tiny.db.elephantsql.com/zlwrrqqh")
 
 # ID dari Channel Atau Group Untuk Wajib Subscribenya
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", " -1002101526284"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", " -1001830849298"))
 FORCE_SUB_GROUP = int(os.environ.get("FORCE_SUB_GROUP", "0"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
